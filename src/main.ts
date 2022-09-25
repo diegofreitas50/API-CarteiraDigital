485172896b5203e7d09e2656b06720c3ea8cd98a
Diego Freitas dos Santos <diegofreitas50@hotmail.com>
2022-09-25 00:15:33 -0300

    chore: swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+
+  app.set('trust proxy', 1);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('API-Carteira-Digiral')
+    .setDescription('Carteira digital')
+    .setVersion('1.0.0')
+    .addTag('Status')
+    .addTag('Auth')
+    .addTag('Users')
+    .addTag("Transaction")
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
