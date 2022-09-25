feebcdfef6444eaa73d3ed7b5f2bd32bb9c15541
Diego Freitas dos Santos <diegofreitas50@hotmail.com>
2022-09-25 00:15:09 -0300

    feat: app status

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
+@ApiTags('Status')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/status')
+  @ApiOperation({ summary: 'Visualizar status da aplicação' })
+  getAppStatus(@Req() req: Request) {
+    const baseUrl = req.protocol + '://' + req.get('host');
+    return this.appService.getAppStatus(baseUrl);
   }
 }
