35ea0d89fb6e5537ba444fd5e32f935a5bf9ef93
Diego Freitas dos Santos <diegofreitas50@hotmail.com>
2022-09-25 02:48:45 -0300

    chore: transactions routes

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,34 +1,57 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { TransactionService } from './transaction.service';
+import {
+  Body, Controller,
+  Get, Param, Patch, Post, UseGuards
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { LoggedUser } from 'src/auth/logged-user.decorator';
+import { User } from 'src/user/entities/user.entity';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { TransactionService } from './transaction.service';
 
+@UseGuards(AuthGuard())
+@ApiBearerAuth()
 @Controller('transaction')
 export class TransactionController {
   constructor(private readonly transactionService: TransactionService) {}
 
-  @Post()
-  create(@Body() createTransactionDto: CreateTransactionDto) {
-    return this.transactionService.create(createTransactionDto);
+  @ApiTags('Transaction')
+  @ApiOperation({
+    summary: 'Realizar uma transação',
+  })
+  @Post('/pay')
+  create(@LoggedUser() user: User, @Body() dto: CreateTransactionDto) {
+    return this.transactionService.create(user, dto);
   }
 
-  @Get()
-  findAll() {
-    return this.transactionService.findAll();
+  @ApiTags('Transaction')
+  @ApiOperation({
+    summary: 'Histórico de todas as transações do usuário logado',
+  })
+  @Get('/extract')
+  findAll(@LoggedUser() user: User) {
+    return this.transactionService.findAll(user);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.transactionService.findOne(+id);
+  @ApiTags('Transaction')
+  @ApiOperation({
+    summary: 'Localizar transação pelo ID',
+  })
+  @Get('/:id')
+  findOne(@LoggedUser() user: User, @Param('id') id: string) {
+    return this.transactionService.findOne(user, id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTransactionDto: UpdateTransactionDto) {
-    return this.transactionService.update(+id, updateTransactionDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.transactionService.remove(+id);
+  @ApiTags('Logista')
+  @ApiOperation({
+    summary: 'Estornar transação por ID (logista)',
+  })
+  @Post('/reversal/:id')
+  update(
+    @LoggedUser() user:User,
+    @Param('id') id: string,
+  ) {
+    return this.transactionService.update(user, id);
   }
 }
--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -1,26 +1,184 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from 'src/user/entities/user.entity';
+import { handleError } from 'src/utils/handleError.util';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
-import { UpdateTransactionDto } from './dto/update-transaction.dto';
 
 @Injectable()
 export class TransactionService {
-  create(createTransactionDto: CreateTransactionDto) {
-    return 'This action adds a new transaction';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(user: User, dto: CreateTransactionDto) {
+    if (user.logist) {
+      throw new UnauthorizedException(
+        'Perfil de logista não tem permissão para esta transação',
+      );
+    }
+
+    const receiver = await this.prisma.user.findUnique({
+      where: { id: dto.payeeID },
+    });
+
+    if (dto.value > user.wallet) {
+      throw new UnauthorizedException(
+        'Você não tem saldo sulficiente para esta transação!',
+      );
+    }
+
+    if (!receiver) {
+      throw new NotFoundException(
+        `Registro com o '${dto.payeeID}' não encontrado.`,
+      );
+    }
+
+    const data: Prisma.TransactionCreateInput = {
+      payerID: user.id,
+      payee: {
+        connect: {
+          id: dto.payeeID,
+        },
+      },
+      value: dto.value,
+    };
+
+    await this.prisma.user.update({
+      where: { id: user.id },
+      data: {
+        wallet: user.wallet - dto.value,
+      },
+    });
+
+    await this.prisma.user.update({
+      where: { id: receiver.id },
+      data: {
+        wallet: receiver.wallet + dto.value,
+      },
+    });
+
+    return this.prisma.transaction
+      .create({
+        data,
+        select: {
+          payerID: true,
+          payee: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+          value: true,
+        },
+      })
+      .catch(handleError);
   }
 
-  findAll() {
-    return `This action returns all transaction`;
+  async findAll(user: User) {
+    const allTransaction = await this.prisma.transaction.findMany({
+      where: { payeeID: user.id, payerID: user.id },
+      select: {
+        payerID: true,
+        payee: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+        value: true,
+      },
+    });
+
+    if (allTransaction.length === 0) {
+      throw new NotFoundException('Não há registros de transações');
+    }
+
+    return allTransaction;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} transaction`;
+  async findOne(user: User, id: string) {
+    const data = await this.prisma.transaction.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        payerID: true,
+        payee: { select: { id: true, name: true } },
+        value: true,
+      },
+    });
+
+    if (user.id !== data.payee.id && user.id !== data.payerID) {
+      throw new UnauthorizedException(
+        'Você não tem permissão para acessar essa transação!',
+      );
+    }
+
+    if (!data) {
+      throw new NotFoundException(`Registro com id '${id}' não encontrado.`);
+    }
+
+    return data;
   }
 
-  update(id: number, updateTransactionDto: UpdateTransactionDto) {
-    return `This action updates a #${id} transaction`;
-  }
+  async update(user: User, id: string) {
+    if (!user.logist) {
+      throw new UnauthorizedException(
+        'Somente logistas têm permissão para esta transação',
+      );
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} transaction`;
+    const transaction = await this.prisma.transaction.findUnique({
+      where: { id: id },
+    });
+    const receiver = await this.prisma.user.findUnique({
+      where: { id: transaction.payerID },
+    });
+    const reversePayer = await this.prisma.user.findUnique({
+      where: { id: transaction.payeeID },
+    });
+
+    const data: Prisma.TransactionCreateInput = {
+      payerID: transaction.payeeID,
+      payee: {
+        connect: {
+          id: transaction.payerID,
+        },
+      },
+      value: transaction.value,
+      isReverse: true,
+    };
+
+    await this.prisma.user.update({
+      where: { id: reversePayer.id },
+      data: {
+        wallet: reversePayer.wallet - transaction.value,
+      },
+    });
+
+    await this.prisma.user.update({
+      where: { id: receiver.id },
+      data: {
+        wallet: receiver.wallet + transaction.value,
+      },
+    });
+
+    return this.prisma.transaction
+      .create({
+        data,
+        select: {
+          payerID: true,
+          payee: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+          value: true,
+        },
+      })
+      .catch(handleError);
   }
 }
